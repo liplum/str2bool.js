d90562e4bb15edd2e44d2d8d92ca50600f29c302
Liplum <Li_plum@outlook.com>
2024-08-28 21:20:32 +0800

    added tests

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
 import test from 'ava'
 import str2bool from "./dist/index.js"
 
-test('"true"', t => {
+test("true", t => {
   t.assert(str2bool("true") === true)
+})
+
+test("false", t => {
+  t.assert(str2bool("false") === false)
+})
+
+test("0", t => {
+  t.assert(str2bool("0") === false)
+})
+
+test("1", t => {
+  t.assert(str2bool("1") === true)
+})
+
+test('""', t => {
+  t.assert(str2bool("") === false)
 })
