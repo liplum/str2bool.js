f09540a956edacfb3488b73c610d969d1f34f9fa
Liplum <Li_plum@outlook.com>
2024-08-28 22:06:15 +0800

    yes or no

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,18 @@ export interface Str2BoolOptions {
    * true by default.
    */
   ignoreCase?: boolean
+
+  /**
+   * If enabled, "yes" and "y" will be considered as `true`,
+   * while "no" and "n" will be considered as `false`.
+   * 
+   * false by default.
+   */
+  yesOrNo?: boolean
+
+  // truthy?: string[]
+
+  // falsy?: string[]
 }
 
 function str2bool(str: string, options?: Str2BoolOptions & { strict: false }): boolean
@@ -52,6 +64,7 @@ function str2bool(str: string, options?: Str2BoolOptions): boolean | undefined {
   const strict = options?.strict ?? false
   const trim = options?.trim ?? true
   const ignoreCase = options?.ignoreCase ?? true
+  const yesOrNo = options?.yesOrNo ?? false
   if (trim) {
     str = str.trim()
   }
@@ -63,6 +76,25 @@ function str2bool(str: string, options?: Str2BoolOptions): boolean | undefined {
   if (str.length == 5) {
     if (ignoreCase ? str.toLowerCase() == "false" : str == "false")
       return false
+  }
+
+  if (yesOrNo) {
+    if (str.length == 3) {
+      if (ignoreCase ? str.toLowerCase() == "yes" : str == "yes")
+        return true
+    }
+    if (str.length == 1) {
+      if (ignoreCase ? str.toLowerCase() == "y" : str == "y")
+        return true
+    }
+    if (str.length == 2) {
+      if (ignoreCase ? str.toLowerCase() == "no" : str == "no")
+        return false
+    }
+    if (str.length == 1) {
+      if (ignoreCase ? str.toLowerCase() == "n" : str == "n")
+        return false
+    }
   }
 
   const maybeFloat = parseFloat(str)
--- test.js
+++ test.js
@@ -1,50 +1,35 @@
 import test from 'ava'
 import str2bool from "./dist/index.js"
 
-test("true", t => {
+test("true and false", t => {
   t.assert(str2bool("true") === true)
-})
-
-test("false", t => {
   t.assert(str2bool("false") === false)
-})
-
-test("True", t => {
   t.assert(str2bool("True") === true)
-})
-
-test("False", t => {
   t.assert(str2bool("False") === false)
 })
 
-test("0", t => {
+test("number", t => {
   t.assert(str2bool("0") === false)
-})
-
-test("1", t => {
   t.assert(str2bool("1") === true)
+  t.assert(str2bool("0.1") === true)
+  t.assert(str2bool("0x00") === false)
 })
 
 test('javascript', t => {
   t.assert(str2bool("javascript") === true)
 })
 
-test('""', t => {
+test('whitespace', t => {
   t.assert(str2bool("") === false)
-})
-
-test('"  "', t => {
   t.assert(str2bool("  ") === false)
-})
-
-test('"  " trim off', t => {
   t.assert(str2bool("  ", { trim: false }) === true)
 })
 
-test("0.1", t => {
-  t.assert(str2bool("0.1") === true)
-})
-
-test("0x00", t => {
-  t.assert(str2bool("0x00") === false)
+test("yes or no", t => {
+  t.assert(str2bool("yes", { yesOrNo: true }) === true)
+  t.assert(str2bool("y", { yesOrNo: true }) === true)
+  t.assert(str2bool("Y", { yesOrNo: true }) === true)
+  t.assert(str2bool("no", { yesOrNo: true }) === false)
+  t.assert(str2bool("n", { yesOrNo: true }) === false)
+  t.assert(str2bool("N", { yesOrNo: true }) === false)
 })
